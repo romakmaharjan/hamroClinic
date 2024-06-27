243ee23eb89ab8f5c550e7b3c92cf4ee4896d484
romak maharjan <romakmaharjan@gmail.com>
2024-06-27 16:11:11 +0545

    upload data of appointment feature code added

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,36 +6,61 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\User;
 use App\Models\Doctor;
+use App\Models\Appointment;
 
 class HomeController extends Controller
 {
     public function redirect()
     {
-        if(Auth::id())
+        if (Auth::id())
         {
-            if(Auth::user()->usertype=='0')
+            if (Auth::user()->usertype == '0')
             {
-                 $doctor =doctor::all();
-                return view('user.home',compact('doctor'));
+                $doctor = Doctor::all();
+                return view('user.home', compact('doctor'));
             }
-            else{
+            else
+            {
                 return view('admin.home');
             }
         }
-        else{
+        else
+        {
             return redirect()->back();
         }
     }
+
     public function index()
     {
-        if(Auth::id())
+        if (Auth::id())
         {
             return redirect('home');
         }
         else
         {
-        $doctor =doctor::all();
-        return view('user.home',compact('doctor'));
+            $doctor = Doctor::all();
+            return view('user.home', compact('doctor'));
         }
+    }
+
+    public function appointment(Request $request)
+    {
+        $data = new Appointment;
+        $data->name = $request->name;
+        $data->email = $request->email;
+        $data->date = $request->date;
+        $data->phone = $request->number;
+        $data->message = $request->message;
+        $data->doctor = $request->doctor;
+        $data->status = 'In progress';
+
+        if (Auth::id())
+        {
+            $data->user_id = Auth::user()->id;
+        }
+
+        $data->save();
+
+        return redirect()->back()->with('message', 'Appointment Request Successful. We will contact you soon.');
     }
 }
